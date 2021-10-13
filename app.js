1b1ff83b286272aa58991ad69e4ef0639cffc921
Max Mäkipää <max.makipaa@gmail.com>
2021-10-13 10:28:57 +0300

    4.2 working

---
+++ app.js
@@ -0,0 +1,33 @@
+// eslint-disable-next-line no-unused-vars
+const http = require('http')
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogRouter = require('./controllers/blogposts')
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+	.then(() => {
+		logger.info('connected to MongoDB')
+	})
+	.catch((error) => {
+		logger.error('error connection to MongoDB:', error.message)
+	})
+
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+	
+app.use('/api/blogs', blogRouter)
+	
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+	
+module.exports = app
+
